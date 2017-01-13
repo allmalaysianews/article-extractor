8d0ff3ed01258d0fad56fc22d2c1852e603096b4
allmalaysianews <website@etlgr.com>
2017-01-14 01:37:48 +0800

    Update parsers.py

--- goose/parsers.py
+++ goose/parsers.py
@@ -20,7 +20,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import lxml.html
+import lxml.html as lxmlhtml
 from lxml.html import soupparser
 from lxml import etree
 from copy import deepcopy
